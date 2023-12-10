d492c2af15f7ec9c9de55c6a321245745cf8d1af
InvalidPanda ツ <117936522+InvalidPandaa@users.noreply.github.com>
2023-12-10 22:12:36 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,72 @@
+const Kahoot = require("kahoot.js-latest");
+const prompt = require('prompt-sync')();
+const colors = require('colors');
+
+console.log("\u2588\u2588\u2557  \u2588\u2588\u2557 \u2588\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2557  \u2588\u2588\u2557 \u2588\u2588\u2588\u2588\u2588\u2588\u2557  \u2588\u2588\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557    \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2557  \u2588\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2588\u2557   \u2588\u2588\u2588\u2557\u2588\u2588\u2588\u2557   \u2588\u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2557 \r\n\u2588\u2588\u2551 \u2588\u2588\u2554\u255D\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2551  \u2588\u2588\u2551\u2588\u2588\u2554\u2550\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2554\u2550\u2550\u2550\u2588\u2588\u2557\u255A\u2550\u2550\u2588\u2588\u2554\u2550\u2550\u255D    \u2588\u2588\u2554\u2550\u2550\u2550\u2550\u255D\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2588\u2588\u2551\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2588\u2588\u2551\u2588\u2588\u2554\u2550\u2550\u2550\u2550\u255D\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\r\n\u2588\u2588\u2588\u2588\u2588\u2554\u255D \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2551\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2551\u2588\u2588\u2551   \u2588\u2588\u2551\u2588\u2588\u2551   \u2588\u2588\u2551   \u2588\u2588\u2551       \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255D\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2551\u2588\u2588\u2554\u2588\u2588\u2588\u2588\u2554\u2588\u2588\u2551\u2588\u2588\u2554\u2588\u2588\u2588\u2588\u2554\u2588\u2588\u2551\u2588\u2588\u2588\u2588\u2588\u2557  \u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255D\r\n\u2588\u2588\u2554\u2550\u2588\u2588\u2557 \u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2551\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2551\u2588\u2588\u2551   \u2588\u2588\u2551\u2588\u2588\u2551   \u2588\u2588\u2551   \u2588\u2588\u2551       \u255A\u2550\u2550\u2550\u2550\u2588\u2588\u2551\u2588\u2588\u2554\u2550\u2550\u2550\u255D \u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2551\u2588\u2588\u2551\u255A\u2588\u2588\u2554\u255D\u2588\u2588\u2551\u2588\u2588\u2551\u255A\u2588\u2588\u2554\u255D\u2588\u2588\u2551\u2588\u2588\u2554\u2550\u2550\u255D  \u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\r\n\u2588\u2588\u2551  \u2588\u2588\u2557\u2588\u2588\u2551  \u2588\u2588\u2551\u2588\u2588\u2551  \u2588\u2588\u2551\u255A\u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255D\u255A\u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255D   \u2588\u2588\u2551       \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2551\u2588\u2588\u2551     \u2588\u2588\u2551  \u2588\u2588\u2551\u2588\u2588\u2551 \u255A\u2550\u255D \u2588\u2588\u2551\u2588\u2588\u2551 \u255A\u2550\u255D \u2588\u2588\u2551\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2551  \u2588\u2588\u2551\r\n\u255A\u2550\u255D  \u255A\u2550\u255D\u255A\u2550\u255D  \u255A\u2550\u255D\u255A\u2550\u255D  \u255A\u2550\u255D \u255A\u2550\u2550\u2550\u2550\u2550\u255D  \u255A\u2550\u2550\u2550\u2550\u2550\u255D    \u255A\u2550\u255D       \u255A\u2550\u2550\u2550\u2550\u2550\u2550\u255D\u255A\u2550\u255D     \u255A\u2550\u255D  \u255A\u2550\u255D\u255A\u2550\u255D     \u255A\u2550\u255D\u255A\u2550\u255D     \u255A\u2550\u255D\u255A\u2550\u2550\u2550\u2550\u2550\u2550\u255D\u255A\u2550\u255D  \u255A\u2550\u255D\r\n                                                                                                                     ".magenta)
+console.log(`\nMade by invalidpandaa\n\n`.bold.red);
+
+const pin = prompt("Game-PIN: ".bold.gray);
+const amount = prompt("Amount of bots: ".bold.gray);
+const name = prompt("Name of the bots: ".bold.gray);
+
+let sent = 1;
+let gameStartedAlertDisplayed = false;
+let gameEndedAlertDisplayed = false;
+
+const joinBotsSequentially = async () => {
+    const joinBot = async () => {
+        const sentClient = name + (sent++);
+        const client = new Kahoot();
+
+        try {
+            await client.join(pin, sentClient);
+
+            client.on("QuestionStart", async (question) => {
+
+                const possibleAnswers = question.numberOfChoices
+                const randomIndex = Math.floor(Math.random() * possibleAnswers);
+
+                // Wait between 2 and 4 seconds before answering
+                const delay = Math.round(2000 + Math.random() * 2000);
+                console.log(`[${sentClient}] Waiting for ${delay} milliseconds before answering...`.gray);
+
+                setTimeout(async () => {
+                    try {
+                        await client.answer(randomIndex);
+                        console.log(`[${sentClient}] Answered choice ${randomIndex} randomly`.yellow);
+                    } catch (answerErr) {
+                        console.log(`[${sentClient}] Failed to answer: ${answerErr.message}`.red);
+                    }
+                }, delay);
+            });
+
+            client.on("QuizStart", () => {
+                if (!gameStartedAlertDisplayed) {
+                    console.log(`[INFO] Game has started!`.cyan.bold);
+                    gameStartedAlertDisplayed = true;
+                }
+            });
+
+            client.on("QuizEnd", () => {
+                if (!gameEndedAlertDisplayed) {
+                    console.log(`[INFO] Game has ended!`.cyan.bold);
+                    gameEndedAlertDisplayed = true;
+                }
+            });
+
+            console.log(`[${sentClient}] Joined!`.green);
+        } catch (err) {
+            console.log(`[${sentClient}] Failed to join: ${err.description || err.status}`.red);
+        }
+    };
+
+    // Start joining bots sequentially
+    for (let i = 0; i < amount; i++) {
+        await joinBot();
+    }
+
+    console.log("[INFO] All bots joined!".cyan);
+};
+
+console.log(`Joining Kahoot with ${amount} bots... (Press Ctrl+C to exit)`.cyan.bold);
+joinBotsSequentially();
